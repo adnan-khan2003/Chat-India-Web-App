5ec93e02ce85a91c3ca601434a54dd9a5595112b
ADNAN KHAN <134621053+adnan-khan2003@users.noreply.github.com>
2023-09-16 19:04:25 +0530

    Update Register.jsx

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -80,7 +80,7 @@ const Register = () => {
             className="loginorsignup">Something went wrong</span>}
         </form>
         <p className="loginorsignup">
-          You do have an account? <Link to="/login">Login</Link>
+          You do have an account? <Link to="Chat-India-Web-App/login">Login</Link>
         </p>
       </div>
     </div>
