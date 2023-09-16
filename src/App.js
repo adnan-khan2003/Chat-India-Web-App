66707a2457df5f6b94c7cc2c5f8d6c403824a370
ADNAN KHAN <134621053+adnan-khan2003@users.noreply.github.com>
2023-09-16 11:05:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 
   const ProtectedRoute = ({children}) => {
     if(!currentUser){
-      return <Navigate to='/login'/>
+      return <Navigate to='/Chat-India-Web-App'/>
     }
 
     return children
@@ -23,7 +23,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/'>
+          <Route path='/Chat-India-Web-App'>
             <Route index element={
             <ProtectedRoute>
               <Home />
