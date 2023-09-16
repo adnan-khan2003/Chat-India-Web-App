45c4f787d6fa5ed8162692aa354e4ca93c117c60
ADNAN KHAN <134621053+adnan-khan2003@users.noreply.github.com>
2023-09-16 18:57:10 +0530

    Update Chat.jsx

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,35 +1,3 @@
-/*
-import React, { useContext } from 'react'
-import cam from '../Images/cam.svg'
-import add from '../Images/add.svg'
-import more from '../Images/more.svg'
-import Messages from './Messages'
-import Input from './Input'
-import { ChatContext } from '../context/ChatContext'
-
-const Chat = () => {
-  const {data} = useContext(ChatContext)
-
-  return (
-    <div className='chat'>
-        <div className="chatinfo">
-          <div>{data.user?.displayName}</div>
-          <div className="chaticons">
-            <img src={cam} alt="video call" />
-            <img src={add} alt="add friend" />
-            <img src={more} alt="more" />
-          </div>
-        </div>
-        <Messages/>
-        <Input/>
-    </div>
-  )
-}
-
-export default Chat
-*/
-
-
 import React, { useContext } from "react";
 import cam from '../Images/cam.svg'
 import add from '../Images/add.svg'
